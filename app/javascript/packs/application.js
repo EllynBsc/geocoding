4d8c3543436cc08efa9e7fa3010a0c1fffe6ff46
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2019-12-04 10:58:08 +0100

    finished lecture

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,13 @@
 import "bootstrap";
+
+import 'mapbox-gl/dist/mapbox-gl.css'; // import the css of the package mapbox-gl
+import { initMapbox } from '../plugins/init_mapbox' // import the function file
+initMapbox(); //invoke the function
+
+
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+// adding the css of the search bar of the map
+
+import { initAutocomplete } from '../plugins/init_autocomplete'
+
+initAutocomplete(); //invoke the function initAutocomplete
